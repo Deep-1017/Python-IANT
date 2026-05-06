8a4c2069a57425472faaa38b07d882e435d32e46
DeepKansara1212 <deepkansara1212@gmail.com>
2026-05-06 11:53:02 +0530

    Add Python Basics

--- 01_Intro/main.py
+++ 01_Intro/main.py
@@ -1,5 +1,40 @@
-print("Hello World")
+# print("Hello, World!", end=" $ ")
+# print("Hii", end=" $ ")
+# print("How are you?") 
 
-if 1 == 1:
-    # code
-    print("Condition is true")
+if (10 > 20):
+    print("10 > 20")
+
+
+# Variable Define
+name = "Deep"
+Name = "John"
+# print(name) 
+
+# print(123 + 12) 
+
+# print("Hello, I am ", 23, " Years old")
+
+
+# firstName = "Alice"
+# FirstName = "John"
+# first_name = "Ann"
+
+first_name, firstName, FirstName = "Jonh", "Alice", "Ann"
+
+# global variables
+a = b = c = 10
+
+def greet():
+    global d, name, a
+    d = 45
+    name = "hi"
+    a = 12
+    print("Inside func", a)
+
+
+greet()
+# print(d)
+# print(name)
+
+print("Outside func", a)
