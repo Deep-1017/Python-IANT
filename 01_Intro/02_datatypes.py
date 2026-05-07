0c1511137d11330f75de443cf0779cf4057b5821
DeepKansara1212 <deepkansara1212@gmail.com>
2026-05-07 11:43:14 +0530

    Add Python Basics

---
+++ 01_Intro/02_datatypes.py
@@ -0,0 +1,23 @@
+# int, float, str, bool
+
+# age = True
+# print(type(age)) 
+
+
+# Type Conversion
+num = "32"
+# print(num)
+# print(type(num))
+
+# print("------ After Conversion ----")
+
+numToStr = complex(num) 
+# print(numToStr)
+# print(type(numToStr))
+
+# Falsy Values => 0, "", [], (), {}, false 
+
+
+
+# Complex No. 
+# complexNo = 3 + 2j
---
+++ 01_Intro/03_random.py
@@ -0,0 +1,6 @@
+# Built in module / Library / Package 
+
+import random
+
+random_number = random.randrange(1, 10) 
+print(random_number)
