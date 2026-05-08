a7cdd03bc20b8718f3058c1f29953bed8c7ef8fe
DeepKansara1212 <deepkansara1212@gmail.com>
2026-05-08 11:35:08 +0530

    Add Python Strings

---
+++ 02_Strings/01_main.py
@@ -0,0 +1,11 @@
+name = 'Hi! My name is "Python", ' \
+'I am a programming language, ' \
+'I am a interpreted language'
+
+# Multiline Stinrgs
+para = """
+    Hi! My name is "Python
+    I am a programming language
+    I am a interpreted language
+"""
+print(para)
