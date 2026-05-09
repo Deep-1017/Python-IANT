a114113777e5a000244dffd6a5b46ad38a1574a6
DeepKansara1212 <deepkansara1212@gmail.com>
2026-05-09 11:27:42 +0530

    Add Str Methods

---
+++ 02_Strings/02_Str-Methods.py
@@ -0,0 +1,27 @@
+text = "Hello, World! My name, is Python."
+
+print(text.split(","))
+
+# print(text.replace("Hello", "Hi"))
+# print(text.upper())
+# print(text.lower())
+
+# print(text)
+# print(text.strip())
+
+
+
+# print(text[-10:-5])
+
+# print(text[0:13:2])
+# print(text[8:])
+# print(text[:20])
+# print(text[:])
+
+
+
+lengthOfA = len(text)
+# print(lengthOfA)
+
+# print("Python" in a)
+# print("Python" not in a)
