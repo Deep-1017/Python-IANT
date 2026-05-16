30546972dabca79a8882f06966ad079604de5211
DeepKansara1212 <deepkansara1212@gmail.com>
2026-05-16 10:35:21 +0530

    Add Python Operator

---
+++ 03_Operators/01_main.py
@@ -0,0 +1,15 @@
+# Identity Operators => is, is not
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+# print(x is not z)
+# print(x is y)
+# print(x == y)
+
+
+# Membership Operator => in, not in
+fruits = ["apple", "banana", "cherry"]
+
+print("banana" not in fruits)
