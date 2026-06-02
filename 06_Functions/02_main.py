3c7332651ce8055188f57818c3121b294cef2bba
DeepKansara1212 <deepkansara1212@gmail.com>
2026-06-02 12:07:35 +0530

    Add Python Function

---
+++ 06_Functions/02_main.py
@@ -0,0 +1,7 @@
+my_fruits = ["apple", "banana", "cherry"]
+
+def func1():
+    for fruit in my_fruits:
+        print(fruit)
+        
+func1()
---
+++ 06_Functions/main.py
@@ -0,0 +1,6 @@
+def greet(name, age, city="Anand"):
+    print(f"Good Morning, {name}. You are {age} years old.")
+    return age
+
+greet("Deep", 25)
+
